4c9c19cf3ea11fb319ad119def2950bec6cdd727
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-11-11 20:51:49 +0100

    Add files via upload

---
+++ Liczby_pierwsze.cpp
@@ -0,0 +1,44 @@
+﻿// Liczby_pierwsze.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+bool pierwszy(int p)
+{
+	for (int i = 2; i <=sqrt(p); i++)
+	{
+		if (p % i == 0)
+			return false;
+		return true;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int p;
+		cin >> p;
+		if (pierwszy(p) == true)
+			cout << "TAK" << endl;
+		else
+			cout << "NIE" << endl;
+	}
+	return 0;
+	
+}
+	
+
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
